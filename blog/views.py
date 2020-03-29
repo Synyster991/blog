1fbdb4daefde4eeb685e4380a3dc94543c464d0a
Filip Dimitrievski <f_dimitrievski@outlook.com>
2020-03-29 15:35:24 -0400

    SignUp and login is working

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.http import Http404
+from django.forms.utils import ErrorList
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
     return render(request, 'blog/home.html')
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/sign_up.html'
